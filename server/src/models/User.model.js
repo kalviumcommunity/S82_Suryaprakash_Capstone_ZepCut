251da11f021cea291ec00ecf13328e1122421ca8
surya <suryaprk165@gmail.com>
2025-05-05 10:09:34 +0530

    schema implemented

--- server/src/models/User.model.js
+++ server/src/models/User.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  phone: String,
+  phone: Number,
   role: { type: String, enum: ['salon', 'barber', 'customer'], required: true },
   profilePhotoUrl: String,
   location: {
